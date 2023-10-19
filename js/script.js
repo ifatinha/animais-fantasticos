8aea6bc84f8101e81f20991d783c9834748797a6
ifatinha <ifatinha99@gmail.com>
2023-10-19 19:30:43 -0300

    accordion list function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent .js-animal");
+/** navegação por tabs **/
+function tabNavigation() {
+    const tabMenu = document.querySelectorAll(".js-tabmenu li");
+    const tabContent = document.querySelectorAll(".js-tabcontent .js-animal");
 
-(() => {
     if (tabContent.length && tabMenu.length) {
         tabContent[0].classList.add("ativo");
 
@@ -20,4 +21,29 @@ const tabContent = document.querySelectorAll(".js-tabcontent .js-animal");
             });
         })
     }
+}
+
+/** accordion */
+function accordion() {
+    const accordionList = document.querySelectorAll(".js-accordion dt");
+    const ativeClass = "open-accordion";
+
+    if (accordionList.length) {
+        accordionList[0].classList.add(ativeClass);
+        accordionList[0].nextElementSibling.classList.add(ativeClass);
+
+        function openAccordion() {
+            this.classList.toggle(ativeClass);
+            this.nextElementSibling.classList.toggle(ativeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', openAccordion);
+        })
+    }
+}
+
+(() => {
+    tabNavigation();
+    accordion();
 })();
