0e688d2e9eac9b6ea937a2f4182fd6f8856aba61
ifatinha <ifatinha99@gmail.com>
2023-10-19 20:19:06 -0300

    navegação suave

--- js/script.js
+++ js/script.js
@@ -43,7 +43,28 @@ function accordion() {
     }
 }
 
+/** scroll suave */
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(`${href}`);
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+}
+
 (() => {
     tabNavigation();
     accordion();
+    scrollSuave();
 })();
